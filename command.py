0b452499e98e64a4c0ea1fcd52d6261e178792fc
John Hill <xs.coating@gmail.com>
2019-01-24 01:44:18 -0600

    more mature version of the command executor

--- command.py
+++ command.py
@@ -1,16 +1,19 @@
 from urllib.request import Request, urlopen, URLError
+from node_information import NodeInfo
 import ssl
 import sys
 import util
 import json
 import logging
 import time
-from node_information import NodeInfo
 
 HELP = """
 python3 command.py <mode>
 
-modes: undirected_command
+modes: undirected_command (loop)
+
+With loop specified, the command module will request new commands after completing a command
+automatically, stopping when it receives any error from the Node API
 """
 
 MAX_ATTEMPTS = 5
@@ -30,9 +33,12 @@ class CommandModule:
         self.node_info = NodeInfo(use_logger)
         self.config = util.load_config_from_file()
         self.max_attempts = self.config['max_rpc_tries']
+        self.ssl_ctx = ssl.SSLContext()
+        self.ssl_ctx.load_default_certs()
 
     def _get_block_data(self, block_number, command_id):
-        block_data = node_info.get_block_data(block_number)
+        config = self.config
+        block_data = self.node_info.get_block_data(block_number)
         if block_data:
             output = dict(success=True,
                           command_id=command_id,
@@ -48,7 +54,7 @@ class CommandModule:
             max_attempts = self.max_attempts
             while max_attempts > 0:
                 try:
-                    urlopen(req, context=ssl_context)
+                    urlopen(req, context=self.ssl_ctx)
                     logger.info("Node information updated successfully.")
                 except URLError as err:
                     logger.error("Error from Node API update endpoint: {0}".format(err))
@@ -74,7 +80,7 @@ class CommandModule:
             max_attempts = self.max_attempts
             while max_attempts > 0:
                 try:
-                    urlopen(req, context=ssl_ctx)
+                    urlopen(req, context=self.ssl_ctx)
                     logger.info("Node information updated successfully.")
                     break
                 except URLError as err:
@@ -88,8 +94,7 @@ class CommandModule:
     def undirected_command(self):
         dispatch_undirected_url = self.config["api_endpoint"]
         dispatch_undirected_url += "node_api/dispatch_undirected_command/" + self.config["api_key"]
-        ssl_ctx = ssl.SSLContext()
-        ssl_ctx.load_default_certs()
+        ssl_ctx = self.ssl_ctx
         try:
             response_data = json.load(urlopen(dispatch_undirected_url, context=ssl_ctx))
             if response_data["result"] == "OK":
@@ -106,7 +111,6 @@ class CommandModule:
 
         except URLError as err:
             logger.error("URLError: {0}".format(URLError))
-            return False
 
 
 if __name__ == "__main__":
@@ -118,7 +122,7 @@ if __name__ == "__main__":
         if sys.argv[2] == "loop":
             loop = True
 
-    logger = logging.getLogger("Command Executioner v1")
+    logger = logging.getLogger("Command Executor v1")
     logger.setLevel(logging.INFO)
     ch = logging.StreamHandler()
     ch.setLevel(logging.INFO)
@@ -128,61 +132,14 @@ if __name__ == "__main__":
     # add the handlers to the logger
     logger.addHandler(ch)
 
-    node_info = NodeInfo(logger)
+    command_module = CommandModule(logger)
     if mode == "undirected_command":
-        config = util.load_config_from_file()
-        url = config["api_endpoint"] + "node_api/dispatch_undirected_command/" + config["api_key"]
-        ssl_context = ssl.SSLContext()
-        ssl_context.load_default_certs()
-        try:
-            remote_file = urlopen(url)
-            response_data = json.load(remote_file)
-            if response_data["result"] == "OK":
-                command_data = response_data["command_data"]
-                if 'get_block_data' in command_data:
-                    command_id = command_data["command_id"]
-                    block_data = node_info.get_block_data(command_data['get_block_data'])
-                    if block_data:
-                        output = dict(success=True,
-                                      command_id=command_id,
-                                      input=str(block_data))
-                        api_endpoint_url = config["api_endpoint"] + "node_api/command_output/" + config["api_key"]
-                        logger.debug("Making request to api_endpoint: " + api_endpoint_url)
-
-                        req = Request(api_endpoint_url,
-                                      data=json.dumps(output).encode('utf-8'),
-                                      headers={'Content-Type': 'application/json',
-                                               'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'},
-                                      method="POST")
-                        try:
-                            response = urlopen(req, context=ssl_context)
-                            logger.info("Node information updated successfully.")
-                        except URLError as err:
-                            logger.error("Error from Node API update endpoint: {0}".format(err))
-                            error_delay = config['polling_interval']
-                            logger.info("Sleeping for {0} seconds".format(error_delay))
-                            time.sleep(error_delay)
-                    else:
-                        output = dict(success=False,
-                                      command_id=command_id,
-                                      input="",
-                                      error_message="Could not get block data.")
-                        api_endpoint_url = config["api_endpoint"] + "node_api/command_output/" + config["api_key"]
-                        logger.debug("Making request to api_endpoint: " + api_endpoint_url)
-
-                        req = Request(api_endpoint_url,
-                                      data=json.dumps(output).encode('utf-8'),
-                                      headers={'Content-Type': 'application/json',
-                                               'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'},
-                                      method="POST")
-                        try:
-                            response = urlopen(req, context=ssl_context)
-                            logger.info("Node information updated successfully.")
-                        except URLError as err:
-                            logger.error("Error from Node API update endpoint: {0}".format(err))
-                            error_delay = config['polling_interval']
-                            logger.info("Sleeping for {0} seconds".format(error_delay))
-                            time.sleep(error_delay)
-
-        except URLError:
-            logger.error("Error connecting to " + url)
+        if loop:
+            logger.info("Starting undirected command loop.")
+            try:
+                while True:
+                    command_module.undirected_command()
+            except NodeApiError:
+                logger.info("Ending undirected command loop.")
+        else:
+            command_module.undirected_command()
